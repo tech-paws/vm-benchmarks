0da0b5faa3d589c692c1fb3fb3a07667688a21ac
Andrey Kabylin <andrey@kabylin.ru>
2020-11-28 07:31:33 +0000

    draw square

--- vm-benchmarks/src/lib.rs
+++ vm-benchmarks/src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use vm;
+
+pub struct BenchmarkModule {}
+
+impl vm::Module for BenchmarkModule {
+    fn init() {
+        let command = vm::data::Command::empty(vm::commands::gapi::SET_COLOR_PIPELINE);
+        vm::push_command(command, vm::Source::GAPI);
+    }
+
+    fn drop() {}
+
+    fn step() {}
+
+    fn render() {
+        let points = [
+            vm::data::Vec2f::new(0.0, 0.0),
+            vm::data::Vec2f::new(100.0, 0.0),
+            vm::data::Vec2f::new(100.0, 100.0),
+            vm::data::Vec2f::new(0.0, 100.0),
+        ];
+
+        let command_payload = unsafe { vm::data::CommandPayload::new(&points) };
+        let command = vm::data::Command::new(vm::commands::gapi::DRAW_PATH, command_payload);
+
+        vm::push_command(command, vm::Source::GAPI);
     }
 }
