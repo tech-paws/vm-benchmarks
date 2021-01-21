e03ae9afd47c841f9ef3e275e0277775f2bff2ba
Andrey Kabylin <andrey@kabylin.ru>
2021-01-21 18:58:20 +0000

    update vm ref

--- vm-benchmarks/src/modules/quads.rs
+++ vm-benchmarks/src/modules/quads.rs
@@ -1,7 +1,7 @@
 use vm::{
     commands,
-    commands_bus::Source,
-    data::{Command, BytesBuffer, Vec2f},
+    commands::Source,
+    data::{BytesBuffer, Command, Vec2f, Vec4f},
     module::{Module, ModuleState, CLIENT_ID},
 };
 
@@ -14,11 +14,7 @@ impl QuadsModule {
 }
 
 impl Module for QuadsModule {
-    fn init(&mut self, state: &mut ModuleState) {
-        let command = Command::empty(commands::gapi::SET_COLOR_PIPELINE);
-        let commands_bus = &state.commands_bus;
-        commands_bus.push_command(CLIENT_ID, command, Source::GAPI);
-    }
+    fn init(&mut self, _: &mut ModuleState) {}
 
     fn shutdown(&mut self, _: &mut ModuleState) {}
 
@@ -27,14 +23,20 @@ impl Module for QuadsModule {
     fn render(&mut self, state: &mut ModuleState) {
         let commands_bus = &state.commands_bus;
 
-        let points = [
-            Vec2f::new(0.0, 0.0),
-            Vec2f::new(100.0, 0.0),
+        let color = Vec4f::new(1.0, 0.0, 0.0, 1.0);
+        let command_payload = BytesBuffer::new(&[color]);
+        let command = Command::new(commands::gapi::SET_COLOR_PIPELINE, command_payload);
+
+        commands_bus.push_command(CLIENT_ID, command, Source::GAPI);
+
+        let args = [
+            // Position
             Vec2f::new(100.0, 100.0),
-            Vec2f::new(0.0, 100.0),
+            // Size
+            Vec2f::new(100.0, 100.0),
         ];
 
-        let command_payload = BytesBuffer::new(&points);
+        let command_payload = BytesBuffer::new(&args);
         let command = Command::new(commands::gapi::DRAW_QUADS, command_payload);
 
         commands_bus.push_command(CLIENT_ID, command, Source::GAPI);
