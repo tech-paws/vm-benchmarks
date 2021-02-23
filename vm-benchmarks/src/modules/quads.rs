3b80b96bc137911258a4892155bb1f63ec692fc6
Andrey Kabylin <andrey@kabylin.ru>
2021-02-23 15:50:58 +0000

    Render text

--- vm-benchmarks/src/modules/quads.rs
+++ vm-benchmarks/src/modules/quads.rs
@@ -11,14 +11,10 @@ pub struct QuadsModule {
     camera_matrices: CameraMatrices,
     camera_transform: OthroCameraTransforms,
     quad_transforms: Transforms2D,
+    text_transforms: Transforms2D,
     quad_model_matrix: Mat4f,
     quad_mvp_matrix: Mat4f,
-}
-
-#[repr(C)]
-pub struct TextCommandPayload {
-    pub pos: Vec2f,
-    pub text: BytesBuffer,
+    text_mvp_matrix: Mat4f,
 }
 
 impl QuadsModule {
@@ -35,9 +31,44 @@ impl QuadsModule {
                 scaling: Vec2f::new(1., 1.),
                 rotation: 0.,
             },
+            text_transforms: Transforms2D {
+                position: Vec2f::ZERO,
+                scaling: Vec2f::new(1., 1.),
+                rotation: 0.,
+            },
             quad_model_matrix: Mat4f::IDENT,
             quad_mvp_matrix: Mat4f::IDENT,
+            text_mvp_matrix: Mat4f::IDENT,
         }
+    }
+}
+
+impl QuadsModule {
+    fn update_camera(&mut self) {
+        self.camera_matrices = create_ortho_camera_matrices(self.camera_transform);
+    }
+
+    fn update_quad(&mut self, state: &mut ModuleState) {
+        self.quad_transforms.position = Vec2f::new(
+            self.camera_transform.viewport_size.x / 2.,
+            self.camera_transform.viewport_size.y / 2.,
+        );
+
+        self.quad_transforms.scaling = Vec2f::new(430., 600.);
+        self.quad_transforms.rotation += 0.25 * state.delta_time;
+
+        self.quad_model_matrix = create_2d_model_matrix(self.quad_transforms);
+        self.quad_mvp_matrix = self.camera_matrices.mvp_matrix * self.quad_model_matrix;
+    }
+
+    fn update_text(&mut self) {
+        self.text_transforms.position =
+            Vec2f::new(10.0, self.camera_transform.viewport_size.y - 24.);
+
+        self.text_transforms.scaling = Vec2f::new(1., 1.);
+
+        let model_matrix = create_2d_model_matrix(self.text_transforms);
+        self.text_mvp_matrix = self.camera_matrices.mvp_matrix * model_matrix;
     }
 }
 
@@ -49,18 +80,9 @@ impl Module for QuadsModule {
     fn step(&mut self, state: &mut ModuleState) {
         let commands = state.get_commands(Source::Processor);
 
-        self.camera_matrices = create_ortho_camera_matrices(self.camera_transform);
-
-        self.quad_transforms.position = Vec2f::new(
-            self.camera_transform.viewport_size.x / 2.,
-            self.camera_transform.viewport_size.y / 2.,
-        );
-
-        self.quad_transforms.scaling = Vec2f::new(430., 600.);
-        self.quad_transforms.rotation -= 0.25 * state.delta_time;
-
-        self.quad_model_matrix = create_2d_model_matrix(self.quad_transforms);
-        self.quad_mvp_matrix = self.camera_matrices.mvp_matrix * self.quad_model_matrix;
+        self.update_camera();
+        self.update_quad(state);
+        self.update_text();
     }
 
     fn render(&mut self, state: &mut ModuleState) {
@@ -69,24 +91,24 @@ impl Module for QuadsModule {
         let color = Vec4f::new(1.0, 1.0, 0.0, 1.0);
         let command_payload = &[BytesBuffer::new(&[color])];
         let command = Command::new(commands::gapi::SET_COLOR_PIPELINE, command_payload);
-
         commands_bus.push_command(CLIENT_ID, command, Source::GAPI);
 
         let command_payload = &[BytesBuffer::new(&[self.quad_mvp_matrix])];
         let command = Command::new(commands::gapi::DRAW_CENTERED_QUADS, command_payload);
+        commands_bus.push_command(CLIENT_ID, command, Source::GAPI);
 
-         commands_bus.push_command(CLIENT_ID, command, Source::GAPI);
+        let command_payload = &[BytesBuffer::new(&[0u64])];
+        let command = Command::new(commands::gapi::SET_TEXTURE_PIPELINE, command_payload);
+        commands_bus.push_command(CLIENT_ID, command, Source::GAPI);
 
         let frame_time = format!("Frame Time: {:?}", state.last_time.elapsed());
-
         let command_payload = &[
             /* font id */ // TODO
             /* font size */ BytesBuffer::new::<u32>(&[12]),
-            /* text position */ BytesBuffer::new(&[Vec2f::new(100., 100.)]),
+            /* mvp matrix */ BytesBuffer::new(&[self.text_mvp_matrix]),
             /* text */ BytesBuffer::from_string(&frame_time),
         ];
         let command = Command::new(commands::gapi::DRAW_TEXTS, command_payload);
-
         commands_bus.push_command(CLIENT_ID, command, Source::GAPI);
     }
 }
